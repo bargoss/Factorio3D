5b822b6c56ca81c9400289a04adb05164e72309f
bargoss <baranyesiltepe98@gmail.com>
2019-06-08 15:11:57 +0300

    fixed ui bug
    remove conveyors use output pipes

--- Assets/BuildController.cs
+++ Assets/BuildController.cs
@@ -18,9 +18,25 @@ public class BuildController : MonoBehaviour
 
     public FloatEvent selectBlockTypeEvent = new FloatEvent();
 
-    // Start is called before the first frame update
-    void Start()
+    Quaternion[] rotations;
+    int targetRotationIndex = 0;
+    Quaternion TargetRotation
     {
+        get
+        {
+            return rotations[targetRotationIndex];
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rotations = new Quaternion[4];
+        rotations[0] = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        rotations[1] = Quaternion.LookRotation(Vector3.left, Vector3.up);
+        rotations[2] = Quaternion.LookRotation(Vector3.back, Vector3.up);
+        rotations[3] = Quaternion.LookRotation(Vector3.right, Vector3.up);
+
         selectedBlockType = 5;
         gamePlane = new Plane(Vector3.up, Vector3.up);
 
@@ -30,13 +46,20 @@ public class BuildController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Utility.GetMousePosInWorldSpace(gamePlane, camera);
+        Vector3 mousePositionActual = mousePosition + Vector3.one * 0.5f;
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Utility.GetMousePosInWorldSpace(gamePlane, camera);
-            mousePosition += Vector3.one * 0.5f;
-            print("put block: " + selectedBlockType + ", at: " + mousePosition);
-            SetBlock(mousePosition, Quaternion.LookRotation(Vector3.forward, Vector3.up), selectedBlockType);
+            print("put block: " + selectedBlockType + ", at: " + mousePositionActual);
+            SetBlock(mousePositionActual, TargetRotation, selectedBlockType);
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            targetRotationIndex++;
+            targetRotationIndex = targetRotationIndex % 4;
+        }
+
+        MouseHoverStuff(mousePositionActual);
 
         CameraMovement();
     }
@@ -46,12 +69,12 @@ public class BuildController : MonoBehaviour
         blocksList.ResetList();
         for (int i = 0; i < itemsContainer.items.Length; i++)
         {
-            int aasdas = i;
             Item item = itemsContainer.items[i];
             if (item != null && item.blockMesh != null)
             {
+                int aasdas = i;
                 GameObject content = blocksList.CreateContent();
-                content.GetComponentInChildren<Button>().onClick.AddListener(delegate { SelectBlockType(i); });
+                content.GetComponentInChildren<Button>().onClick.AddListener(delegate { SelectBlockType(aasdas); });
                 Text text = content.GetComponentInChildren<Text>();
                 text.text = item.itemName + ", " + i;
             }
@@ -60,9 +83,18 @@ public class BuildController : MonoBehaviour
 
     public void SelectBlockType(int blockType)
     {
+        print("selecting: " + blockType);
         selectedBlockType = blockType;
     }
-
+    void MouseHoverStuff(Vector3 mousePos)
+    {
+        Vector3 blockPos = mousePos.ToVector3Int();
+        Item item = itemsContainer.items[selectedBlockType];
+        Quaternion rotation = rotations[targetRotationIndex];
+        Matrix4x4[] matrices = new Matrix4x4[1];
+        matrices[0] = Matrix4x4.TRS(blockPos, rotation, Vector3.one * 0.5f);
+        Graphics.DrawMeshInstanced(item.blockMesh, 0, item.blockMaterial, matrices);
+    }
     void CameraMovement()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
